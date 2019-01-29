240829943ea0c004de06ddeb60f248dc537a5307
Nikola Kraljevic <K8007@mail.ru>
2019-01-29 20:08:25 +0100

    Main Menu finished

---
+++ Real Math/Assets/My Scripts/MainMenu.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityEngine.SceneManagment;
+
+public class MainMenu : MonoBehaviour
+{
+   public void QuitGame()
+    {
+        Debug.Log("Wyjscie!");
+        Application.Quit();
+    }
+}
